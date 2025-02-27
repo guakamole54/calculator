00f5063227a626f0bf9319663618085183d00120
Robert Mudron <robino1498@gmail.com>
2025-02-27 09:30:41 +0100

    Create operate function that calls sub operations

--- script.js
+++ script.js
@@ -14,6 +14,23 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(operand1, operator, operand2) {
+    operand1 = Number.parseInt(operand1);
+    operand2 = Number.parseInt(operand2);
+    switch (operator) {
+        case ('+'):
+            return add(operand1, operand2);
+        case ('-'):
+            return subtract(operand1, operand2);
+        case ('*'):
+            return multiply(operand1, operand2);
+        case ('/'):
+            return divide(operand1, operand2);
+        default:
+            console.log(`Unknown operator ${operator}`);
+    }
+}
+
 let operand1 = null;
 let operator = null;
 let operand2 = null;
